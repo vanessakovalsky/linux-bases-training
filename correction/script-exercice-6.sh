413060836dbdb10b0615e4f1936667c5554766f6
Vanessa Kovalsky <vanessa.kovalsky@free.fr>
2021-02-09 16:47:15 +0100

    Fix correction exercice 6

--- correction/script-exercice-6.sh
+++ correction/script-exercice-6.sh
@@ -15,17 +15,25 @@ echo 'Rechercher une chaine avec Grep'
 cp /usr/include/stdio.h . 
 grep -nRi define stdio.h
 grep -nRi $(who) /etc/passwd 
-find . -type f -exec grep hello {} \;
+#find . -type f -exec grep hello {} \;
 
 echo 'Trier avec sort'
-
+echo 'repertoire user'
 cut -f 6 -d ':' /etc/passwd
-cut -d: -f 7 passwd|sort|uniq|wc -l
+echo 'bash user'
+cut -d: -f 7 /etc/passwd|sort|uniq|wc -l
+
+echo 'Afficher le carnet d’adresse trié par ordre alphabétique de noms.'
 sort telephone.txt
+echo 'Afficher le nombre de personnes dans le répertoire.'
 wc -l telephone.txt
+echo 'Afficher toutes les lignes concernant les “Dupond”.'
 grep Dupond telephone.txt
-cat telephone.txt | grep -v "Dupond"
-grep Dupond telephone.txt|uniq -f 1|cut -d\; -f 3
-sort telephone.txt|grep Dupond |uniq -f 1|cut -d\; -f 3
+echo 'Afficher toutes les lignes ne concernant pas les Dupond'
+grep -vn "Dupond" telephone.txt
+echo 'Afficher le numéro de téléphone (sans le nom) du premier “Dupond” apparaissant dans le répertoire.'
+grep Dupond telephone.txt|head -n 1|cut	-f 3
+echo 'Afficher le numéro de téléphone (sans le nom) du premier “Dupond” dans l’ordre alphabétique (ordre basé sur les prénoms).'
+sort telephone.txt|grep Dupond |head -n 1|cut	-f 3
 
 
